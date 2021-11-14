e9f26c7e9b532ba72dbafac03924cb790b1839ef
Ivanpepp <nastroivan3@gmail.com>
2021-11-14 14:37:20 +0100

    Faker seed completed

--- database/seeds/TripsTableSeeder.php
+++ database/seeds/TripsTableSeeder.php
@@ -4,6 +4,8 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Str;
 use App\Models\Trip;
 
+use Faker\Generator as Faker;
+
 class TripsTableSeeder extends Seeder
 {
     /**
@@ -11,50 +13,21 @@ class TripsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
         //
-        $trips =[
-            [
-                'title' => 'Prova del Seeder caricato manualmente',
-                'location' =>  'Seeder Land',
-                'description' => 'luogo dei seeder',
-                'url' => 'https://www.thecommandbrick.net/wp-content/uploads/2020/08/Come-Vedere-il-Seed-Minecraft.png',
-                'title_review' => 'Bello il Seeder',
-                'review' => 'molto comodo da usare',
-                'date_review' => '2020/12/04',
-            ],
-            [
-                'title' => 'Prova del Seeder caricato manualmente 2',
-                'location' =>  'Seeder Land 2',
-                'description' => 'luogo dei seeder 2',
-                'url' => 'https://www.thecommandbrick.net/wp-content/uploads/2020/08/Come-Vedere-il-Seed-Minecraft.png',
-                'title_review' => 'Bello il Seeder 2',
-                'review' => 'molto comodo da usare 2',
-                'date_review' => '2020/12/06',
-            ],
-            [
-                'title' => 'Prova del Seeder caricato manualmente 3',
-                'location' =>  'Seeder Land 3',
-                'description' => 'luogo dei seeder 3',
-                'url' => 'https://www.thecommandbrick.net/wp-content/uploads/2020/08/Come-Vedere-il-Seed-Minecraft.png',
-                'title_review' => 'Bello il Seeder 3',
-                'review' => 'molto comodo da usare 3',
-                'date_review' => '2020/12/10',
-            ],
-        ];
-        foreach ($trips as $trip){
-            $newTrip = New Trip();
-            /* $newTrip->title = $trip['title'];
-            $newTrip->location = $trip['location'];
-            $newTrip->description = $trip['description'];
-            $newTrip->url = $trip['url'];
-            $newTrip->title_review = $trip['title_review'];
-            $newTrip->review = $trip['review'];
-            $newTrip->date_review = $trip['date_review']; */
+        for($i =0; $i< 250; $i++){
+            $newTrip = new Trip();
+            $newTrip->title = $faker->words(5, true);
+            $newTrip->location = $faker->words(2, true);
+            $newTrip->description = $faker->paragraphs(4, true);
+            $newTrip->url = $faker->imageUrl();
+            $newTrip->title_review = $faker->words(3, true);
+            $newTrip->review = $faker->paragraphs(2, true);;
+            $newTrip->date_review = $faker->date();
 
-            $newTrip->fill($trip);
-            $newTrip->slug = Str::slug( $trip['title'], '-' );
+
+            $newTrip->slug=Str::slug($newTrip->title, '-');
             $newTrip->save();
         }
     }
